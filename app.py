d7768dbcd1b7449ee5bc1b9d938909195b8a476a
MingzhePei <540191212@qq.com>
2020-07-22 09:07:46 +0800

    新增前后端传参测试

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def ping_pong():
-    return jsonify('Hello Flask!')     #（jsonify返回一个json格式的数据）
+    return jsonify(request.args.get('ID'))
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
