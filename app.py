b771ffa5535eb43473bda0ceff8295c17bd518a2
xuhao <xhao0403@163.com>
2020-08-15 16:35:26 +0800

    实现从前端上传文件，后端解析文件名与label，并返回给前端

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, request
+# -*- coding:utf-8 -*-
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 import manifoldAlgorithm as mf
+import os
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 CORS(app)
 @app.route('/', methods=['GET','POST'])
@@ -17,9 +21,14 @@ def upload():
     if request.method == 'POST':
         f = request.files['pic_file']
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
-        upload_path = os.path.join(basepath, 'static/uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        upload_path = os.path.join(basepath, 'uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
-        resp = make_response(jsonify("static/uploads/"+f.filename))  # 响应体
+	Labels = mf.getLabel("uploads/"+f.filename)
+	result = {
+		"labels":Labels,
+		"path": "uploads/"+f.filename
+	}
+        resp = make_response(jsonify(result))  # 响应体
         resp.headers["Access-Control-Allow-Origin"] = "*"  # 设置响应头
         return resp
     resp = make_response(jsonify("没有"))  # 响应体
